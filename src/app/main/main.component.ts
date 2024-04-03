3e6168c99ce132b69c73f9675b737e395b25192d
Pouya2781 <pouya.s2781@gmail.com>
2024-04-03 12:26:24 +0330

    [ADD] graph service

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {GraphService} from '../services/graph.service';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements AfterViewInit {
+    @ViewChild('graphContainer') graphContainer!: ElementRef;
 
+    constructor(private graphService: GraphService) {}
+    ngAfterViewInit() {
+        this.graphService.createGraph(this.graphContainer.nativeElement);
+    }
 }
